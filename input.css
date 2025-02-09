10ca1b1af2e579a7d584eee2e25f19eb0eb6cb5e
BugHunter-X <askzenai@gmail.com>
2025-02-09 15:48:05 +0100

    Add input.css file

---
+++ input.css
@@ -0,0 +1,12 @@
+```javascript
+//In your main app.js or equivalent
+import './input.css';
+//Then in your input.css file
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+/*This will import the tailwind utilities into your css file*/
+.my-class{ /*Your class name*/
+  @apply text-center; /*Example*/
+}
+```
